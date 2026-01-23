6283f2824c590981fffc36dc4a54af9b617e92eb
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-23 20:26:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ require("dotenv").config();
 
 const { createClient } = require("./core/client");
 const { loadAll } = require("./core/loader");
+const { log } = require("./core/logger");
 
 async function bootstrap() {
+  if (!process.env.TOKEN) {
+    console.error("[FATAL] TOKEN manquant dans les variables Railway.");
+    process.exit(1);
+  }
+
   const client = createClient();
 
   await loadAll(client);
 
   await client.login(process.env.TOKEN);
+  log("Login lancé (en attente du ready)...");
 }
 
 bootstrap().catch((err) => {
