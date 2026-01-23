a2eaed425709e550382b58a7232477dc2decb304
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-23 19:10:35 +0100

    Update client.js

--- src/core/client.js
+++ src/core/client.js
@@ -1,1 +1,16 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
 
+function createClient() {
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds
+    ],
+    partials: [Partials.Channel]
+  });
+
+  client.commands = new Collection();
+
+  return client;
+}
+
+module.exports = { createClient };
