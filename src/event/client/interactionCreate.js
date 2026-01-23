6032ccd0e8c64da6e18d8c3e0b32967968d6feb0
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-23 20:29:19 +0100

    Update interactionCreate.js

--- src/event/client/interactionCreate.js
+++ src/event/client/interactionCreate.js
@@ -1,23 +1,25 @@
-const { error } = require("../../core/logger");
+const { warn } = require("../../core/logger");
 
 module.exports = {
   name: "interactionCreate",
-
+  once: false,
   async execute(interaction, client) {
-    if (!interaction.isChatInputCommand()) return;
-
-    const cmd = client.commands.get(interaction.commandName);
-    if (!cmd) return;
-
     try {
+      if (!interaction.isChatInputCommand()) return;
+
+      const cmd = client.commands.get(interaction.commandName);
+      if (!cmd) return;
+
       await cmd.execute(interaction, client);
-    } catch (err) {
-      error(err);
-      if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({ content: "Erreur interne.", ephemeral: true });
+    } catch (e) {
+      warn("Erreur interactionCreate:", e);
+
+      // Réponse safe
+      if (interaction.deferred || interaction.replied) {
+        await interaction.followUp({ content: "Une erreur est survenue.", ephemeral: true }).catch(() => {});
       } else {
-        await interaction.reply({ content: "Erreur interne.", ephemeral: true });
+        await interaction.reply({ content: "Une erreur est survenue.", ephemeral: true }).catch(() => {});
       }
     }
-  }
+  },
 };
