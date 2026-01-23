43fe6f4a791940d1d2826e552ec843d64bc3d6c8
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-23 20:03:23 +0100

    Update interactionCreate.js

--- src/event/client/interactionCreate.js
+++ src/event/client/interactionCreate.js
@@ -1,1 +1,23 @@
+const { error } = require("../../core/logger");
 
+module.exports = {
+  name: "interactionCreate",
+
+  async execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const cmd = client.commands.get(interaction.commandName);
+    if (!cmd) return;
+
+    try {
+      await cmd.execute(interaction, client);
+    } catch (err) {
+      error(err);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: "Erreur interne.", ephemeral: true });
+      } else {
+        await interaction.reply({ content: "Erreur interne.", ephemeral: true });
+      }
+    }
+  }
+};
