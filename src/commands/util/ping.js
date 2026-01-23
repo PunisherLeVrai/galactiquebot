fff287d7e96995b755f25d092d04d7f4b3b51181
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-23 20:02:36 +0100

    Update ping.js

--- src/commands/util/ping.js
+++ src/commands/util/ping.js
@@ -1,1 +1,12 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Test de latence du bot"),
+
+  async execute(interaction) {
+    const ms = Date.now() - interaction.createdTimestamp;
+    return interaction.reply({ content: `Pong: ${ms}ms`, ephemeral: true });
+  }
+};
