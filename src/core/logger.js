a7ac7d15532487b83e69b94d34d0524538d0f47e
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-23 19:11:10 +0100

    Update logger.js

--- src/core/logger.js
+++ src/core/logger.js
@@ -1,1 +1,13 @@
+function log(...args) {
+  console.log("[BOT]", ...args);
+}
 
+function warn(...args) {
+  console.warn("[BOT_WARN]", ...args);
+}
+
+function error(...args) {
+  console.error("[BOT_ERROR]", ...args);
+}
+
+module.exports = { log, warn, error };
