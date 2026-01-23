bfc27812df2089f321d4e12e6396b085f853473a
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-23 19:10:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+require("dotenv").config();
 
+const { createClient } = require("./core/client");
+const { loadAll } = require("./core/loader");
+
+async function bootstrap() {
+  const client = createClient();
+
+  await loadAll(client);
+
+  await client.login(process.env.TOKEN);
+}
+
+bootstrap().catch((err) => {
+  console.error("[BOOTSTRAP_ERROR]", err);
+  process.exit(1);
+});
