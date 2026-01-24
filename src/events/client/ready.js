40a0616e3eaee4c48301d5f56e9b6636c30ff50e
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-24 02:09:53 +0100

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,10 +1,12 @@
 // src/events/client/ready.js
 const { log } = require("../../core/logger");
+const { startAutomations } = require("../../core/disposAutomation");
 
 module.exports = {
   name: "ready",
   once: true,
   execute(client) {
     log(`Bot connecté : ${client.user.tag} (XIG BLAUGRANA FC Staff)`);
+    startAutomations(client);
   },
 };
