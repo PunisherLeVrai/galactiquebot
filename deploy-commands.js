20cf1695b50e061e9a8fc63d376cddc86897529f
PunisherLeVrai <dylanbodelot1998@gmail.com>
2026-01-25 12:38:46 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,28 +1,32 @@
 require("dotenv").config();
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
 
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID; // optionnel
+const GUILD_ID = process.env.GUILD_ID;
 
-if (!TOKEN) throw new Error("TOKEN manquant");
-if (!CLIENT_ID) throw new Error("CLIENT_ID manquant");
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-// Même liste que dans index.js (copier-coller)
-const COMMANDS = [
-  new SlashCommandBuilder().setName("ping").setDescription("pong"),
-];
+async function loadCommands() {
+  const cmds = [];
+  const dir = path.join(__dirname, "src", "commands");
+  for (const file of fs.readdirSync(dir)) {
+    if (!file.endsWith(".js")) continue;
+    const cmd = require(path.join(dir, file));
+    cmds.push(cmd.data.toJSON());
+  }
+  return cmds;
+}
 
 (async () => {
-  const rest = new REST({ version: "10" }).setToken(TOKEN);
-
+  const commands = await loadCommands();
   const route = GUILD_ID
     ? Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID)
     : Routes.applicationCommands(CLIENT_ID);
 
-  await rest.put(route, { body: COMMANDS.map((c) => c.toJSON()) });
-  console.log(`[DEPLOY] OK (${GUILD_ID ? "GUILD" : "GLOBAL"})`);
-})().catch((e) => {
-  console.error("[DEPLOY_ERROR]", e);
-  process.exit(1);
-});
+  console.log("Déploiement des commandes…");
+  await rest.put(route, { body: commands });
+  console.log("OK.");
+})();
